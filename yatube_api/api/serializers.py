3ef4f75fb2cd813fce3ca1d87d3774cee111fcf2
Роман Лосев <losik19-94@yandex.ru>
2022-02-14 14:23:53 +0300

    done

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -56,7 +56,8 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follow
         fields = '__all__'
-        validators = [UniqueTogetherValidator(
-            queryset=Follow.objects.all(),
-            fields=('user', 'following'),
-        )]
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+            ),)
